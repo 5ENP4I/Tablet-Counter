3df9e9f1ad655e7e0651a31f1cd1625c20f80d28
SeNpAi <syzonenkod@gmail.com>
2023-03-09 21:45:37 +0100

    NavButtonsColor
    - Nav buttons now can change their color on click and should change "Panels"

--- TabletCounter/MainWindow.xaml.cs
+++ TabletCounter/MainWindow.xaml.cs
@@ -15,14 +15,51 @@ using System.Windows.Shapes;
 
 namespace TabletCounter
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        public static Button[] navButtons = new Button[3];
+        public static Image[] navImages = new Image[3];
+          
+
         public MainWindow()
         {
             InitializeComponent();
+            navButtons[0] = Button0;
+            navButtons[1] = Button1;
+            navButtons[2] = Button2;
+        }
+
+        private void Button0_Click(object sender, RoutedEventArgs e)
+        {
+            for (int i = 0; i < navButtons.Length; i++)
+            {
+                if((Button)sender == navButtons[i])
+                {
+                    navButtons[i].Background = (Brush)FindResource("ByzantineBlue");
+                    ChangePannel((Button)sender);
+                }
+                else
+                {
+                    navButtons[i].Background = (Brush)FindResource("AntiFlashWhite");
+                    navButtons[i].Foreground = (Brush)FindResource("Black");
+                }
+            }
+        }
+
+        public void ChangePannel(Button whichIsPressed)
+        {
+            if (whichIsPressed == navButtons[0])
+            {
+                //Menu1
+            }
+            if (whichIsPressed == navButtons[1])
+            {
+                //Menu2
+            }
+            if (whichIsPressed == navButtons[2])
+            {
+                //Menu3
+            }
         }
     }
 }
